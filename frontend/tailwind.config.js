b986ad1a8c54949e5b623c90758cdf45a1ba9b70
harshbansal7 <harshbansal1036@gmail.com>
2024-12-30 23:16:29 +0530

    update

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-const colors = require('tailwindcss/colors')
+import colors from 'tailwindcss/colors'
 
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
